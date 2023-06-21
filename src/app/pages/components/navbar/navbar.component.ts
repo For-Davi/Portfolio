830dc9561e40c844d1f073a5f110228c9c958ea7
Carlos Davi <carlosdavi004@gmail.com>
2023-06-21 10:43:14 -0300

    More configurations in navbar

--- src/app/pages/components/navbar/navbar.component.ts
+++ src/app/pages/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 import { faSun } from '@fortawesome/free-regular-svg-icons';
+import { faBars, faClose} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +8,26 @@ import { faSun } from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  // Icones
   sunIcon = faSun
+  hamburgerIcon = faBars
+  closeIcon = faClose
+
+  // Variaveis
+  menuOpen:boolean = false
+
+  constructor(){}
+
+  @HostListener('window:resize',['$event'])
+  onWindowResize(event:Event){
+    if(window.innerWidth > 768){
+      this.menuOpen = false
+    }
+  }
+  
+  toogleList():void{
+    this.menuOpen = !this.menuOpen
+  }
+
 }
